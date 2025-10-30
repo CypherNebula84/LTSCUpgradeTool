4792d4219d13a6b0cf9b1c58bd2817653cd08729
CypherNebula84 <154773078+CypherNebula84@users.noreply.github.com>
2025-10-30 16:52:29 -0700

    Add files via upload

--- LTSCUpgradeTool.bat
+++ LTSCUpgradeTool.bat
@@ -20,7 +20,7 @@ echo DISCLAIMER:
 echo   Unofficial. You still need:
 echo     - a "Totally" legally obtained and valid LTSC license key
 echo     - the matching LTSC ISO Edition
-echo     - possably system backup before running this script
+echo     - possibly system backup before running this script
 echo.
 echo ISO LINKS:
 echo   Enterprise LTSC: https://drive.massgrave.dev/en_windows_10_iot_enterprise_ltsc_2019_x64_dvd_a1aa819f.iso
@@ -48,7 +48,6 @@ if "%choice%"=="1" (
     set EditionID=EnterpriseS
     set CurrentBuild=17763
     set CurrentBuildNumber=17763
-    set BuildLabEx=17763.4048.amd64fre.ltcm.190902-1700
     set ReleaseId=1809
     set DisplayVersion=2019 LTSC
 ) else if "%choice%"=="2" (
@@ -56,7 +55,6 @@ if "%choice%"=="1" (
     set EditionID=IoTEnterpriseS
     set CurrentBuild=19044
     set CurrentBuildNumber=19044
-    set BuildLabEx=19044.2364.amd64fre.vb_release_ltcm.210914-1433
     set ReleaseId=21H2
     set DisplayVersion=2021 IoT LTSC
 ) else (
@@ -81,21 +79,20 @@ if %errorlevel% neq 0 (
 :: Apply registry edits with before → after messages
 echo.
 echo Changing registry values... It should not take too long 
-for %%K in EditionID ProductName CurrentBuild CurrentBuildNumber BuildLabEx ReleaseId DisplayVersion InstallationType ProductId (
-    :: Get old value
+for %%K in EditionID ProductName CurrentBuild CurrentBuildNumber ReleaseId DisplayVersion InstallationType ProductId (
+    :: Get old value from current PC
     for /f "tokens=2*" %%A in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v %%K 2^>nul ^| find "%%K"') do set oldval=%%B
     :: Set new value
     if "%%K"=="EditionID" set newval=%EditionID%
     if "%%K"=="ProductName" set newval=%EditionName%
     if "%%K"=="CurrentBuild" set newval=%CurrentBuild%
     if "%%K"=="CurrentBuildNumber" set newval=%CurrentBuildNumber%
-    if "%%K"=="BuildLabEx" set newval=%BuildLabEx%
     if "%%K"=="ReleaseId" set newval=%ReleaseId%
     if "%%K"=="DisplayVersion" set newval=%DisplayVersion%
     if "%%K"=="InstallationType" set newval=Client
     if "%%K"=="ProductId" set newval=00330-80000-00000-AAOEM
-    :: Print change
-    echo Changing %%K from "!oldval!" to "!newval!"
+    :: Print change (skip ProductId)
+    if NOT "%%K"=="ProductId" echo Changing %%K from "!oldval!" to "!newval!"
     :: Apply change
     reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v %%K /t REG_SZ /d "!newval!" /f >nul
 )
@@ -116,7 +113,6 @@ echo 2) Reboot your PC.
 echo 3) Mount the LTSC ISO for the edition you selected.
 echo 4) Open it in Explorer.
 echo 5) Run setup.exe in Windows, choose "Keep files and apps".
-echo 6) Activate using your valid LTSC key.
 echo.
 echo NOTE: If the installer fails due to build/edition mismatch, restore the backup:
 echo    reg import "%BACKUP_DIR%\CurrentVersionBackup.reg"
